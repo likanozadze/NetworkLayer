2b5def4f8cc417c6965389f5a34b845a3183bd20
Lika Nozadze <likanozadze04@gmail.com>
2023-11-20 03:10:33 +0400

    general

--- Sources/NetworkLayer/NetworkLayer.swift
+++ Sources/NetworkLayer/NetworkLayer.swift
@@ -6,51 +6,49 @@
 
 import UIKit
 
-public final class NetworkManager {
+public final class NetworkManager<T: Decodable> {
     static let shared = NetworkManager()
-    
+
     public init() {}
-    
-    public func fetchFact(completion: @escaping (Result<[T], Error>) -> Void) {
-        let urlStr = "https://catfact.ninja/facts?limit=20"
-        
-        guard let url = URL(string: urlStr) else {
+
+    public func fetchData(from urlString: String, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: urlString) else {
             completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
             return
         }
-        
+
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
                 return
             }
-            
+
             guard let data = data else {
                 completion(.failure(NSError(domain: "", code: -2, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
                 return
             }
-            
+
             do {
-                let catResults = try JSONDecoder().decode(CatResults.self, from: data)
-                completion(.success(catResults.data))
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
             } catch {
                 completion(.failure(error))
             }
         }.resume()
     }
-    
+
     public func downloadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
-        guard let url = URL(string: "https://image.tmdb.org/t/p/w500\(urlString)") else {
+        guard let url = URL(string: urlString) else {
             completion(nil)
             return
         }
-        
+
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil, let image = UIImage(data: data) else {
                 completion(nil)
                 return
             }
-            
+
             completion(image)
         }.resume()
     }
