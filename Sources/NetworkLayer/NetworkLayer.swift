8b45212ca65b865f37df1682c2fa1978913d5175
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-25 16:40:21 +0400

    changes

--- Sources/NetworkLayer/NetworkLayer.swift
+++ Sources/NetworkLayer/NetworkLayer.swift
@@ -6,9 +6,12 @@
 
 import Foundation
 
-public enum NetworkError: Error {
+enum NetworkError: Error {
     case invalidURL
+    case networkError(Error)
+    case invalidResponse
     case invalidData
+    case decodingError(Error)
 }
 
 public final class NetworkManager {
@@ -16,32 +19,44 @@ public final class NetworkManager {
     
     // MARK: - Methods
     
-    public func fetchData<T: Decodable>(with urlString: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
-        
-        guard !urlString.isEmpty, let url = URL(string: urlString) else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(.invalidData))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let dataResponse = try JSONDecoder().decode(T.self, from: data)
-                DispatchQueue.main.async {
-                    completion(.success(dataResponse))
-                }
-            } catch {
-                completion(.failure(.invalidData))
-            }
-        }.resume()
-    }
-}
+    func request<T: Decodable>(baseURL: String, apiKey: String, endpoint: String, parameters: [String: Any], completion: @escaping (Result<T, NetworkError>) -> Void) {
+         guard var urlComponents = URLComponents(string: baseURL + endpoint) else {
+             completion(.failure(.invalidURL))
+             return
+         }
+
+         urlComponents.queryItems = parameters.map { URLQueryItem(name: $0.key, value: String(describing: $0.value)) }
+
+         guard let url = urlComponents.url else {
+             completion(.failure(.invalidURL))
+             return
+         }
+
+         var request = URLRequest(url: url)
+         request.addValue(apiKey, forHTTPHeaderField: "X-RapidAPI-Key")
+
+         URLSession.shared.dataTask(with: request) { data, response, error in
+             if let error = error {
+                 completion(.failure(.networkError(error)))
+                 return
+             }
+
+             guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                 completion(.failure(.invalidResponse))
+                 return
+             }
+
+             guard let data = data else {
+                 completion(.failure(.invalidData))
+                 return
+             }
+
+             do {
+                 let decodedData = try JSONDecoder().decode(T.self, from: data)
+                 completion(.success(decodedData))
+             } catch {
+                 completion(.failure(.decodingError(error)))
+             }
+         }.resume()
+     }
+ }
